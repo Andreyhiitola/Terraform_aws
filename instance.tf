52473544c95e0fad5db76a10f14dd16b3e9c7213
Andrey Sagurov <96229387+Andreyhiitola@users.noreply.github.com>
2022-08-05 16:48:28 +0300

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "web-server" {
   ami             = "ami-065deacbcaac64cf2"
   instance_type   = "t2.micro"
   count           = 2
+  key_name        = "mykeypair"
   security_groups = ["${aws_security_group.web-server.name}"]
   user_data       = <<-EOF
 
